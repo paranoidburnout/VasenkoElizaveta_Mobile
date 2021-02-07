6681329d0fd0eb67f798775364003b35290be66d
paranoidburnout <paranoidburnout@gmail.com>
2021-02-07 20:49:23 +0300

    placing test data in a separate class

---
+++ src/main/java/utils/TestData.java
@@ -0,0 +1,7 @@
+package utils;
+
+public class TestData {
+    public static final String USERNAME = "Elith Vasenko";
+    public static final String PASSWORD = "vas05ENKO$$";
+    public static final String EMAIL = "Elizaveta_Vasenko@epam.com";
+}
--- src/test/java/scenarios/nativeMobileTests.java
+++ src/test/java/scenarios/nativeMobileTests.java
@@ -4,19 +4,21 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import setup.BaseTest;
 
+import static utils.TestData.*;
+
 public class nativeMobileTests extends BaseTest {
 
     @Test(groups = {"native"}, description = "This simple test for registering a new account and logging into it")
     public void simpleNativeTest() throws IllegalAccessException, NoSuchFieldException, InstantiationException {
 
         getPo().getWelement("registerBtn").click();
-        getPo().getWelement("email").sendKeys("Elizaveta_Vasenko@epam.com");
-        getPo().getWelement("username").sendKeys("Elith Vasenko");
-        getPo().getWelement("password").sendKeys("vas05ENKO$$");
-        getPo().getWelement("confirmPassword").sendKeys("vas05ENKO$$");
+        getPo().getWelement("email").sendKeys(EMAIL);
+        getPo().getWelement("username").sendKeys(USERNAME);
+        getPo().getWelement("password").sendKeys(PASSWORD);
+        getPo().getWelement("confirmPassword").sendKeys(PASSWORD);
         getPo().getWelement("registerAccount").click();
-        getPo().getWelement("loginEmail").sendKeys("Elizaveta_Vasenko@epam.com");
-        getPo().getWelement("loginPassword").sendKeys("vas05ENKO$$");
+        getPo().getWelement("loginEmail").sendKeys(EMAIL);
+        getPo().getWelement("loginPassword").sendKeys(PASSWORD);
         getPo().getWelement("signInBtn").click();
 
         Assert.assertEquals(getPo().getWelement("budgetActivityTitle").getText(), "BudgetActivity",
